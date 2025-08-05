b6ddd30af928bcb44103a29783a42be95eda5d76
hugo <HUGOMVERA@GMAIL.COM>
2025-08-04 22:42:04 -0700

    This is the previous app-release.aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.blacksnowymanx.todoincomposeversion2"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
